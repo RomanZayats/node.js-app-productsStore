29b67b93220e739c810eeb46b9185a3346ca0cce
Roman Zayats <zayatr01@kits.kfplc.com>
2023-08-10 11:53:09 +0300

    added login validators

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { validationResult } = require("express-validator");
 const { Router } = require("express");
 const bcrypt = require("bcryptjs");
 
-const { registerValidators } = require("../utils/validators");
+const { loginValidators, registerValidators } = require("../utils/validators");
 const User = require("../models/user");
 
 const router = Router();
@@ -22,27 +22,12 @@ router.get("/logout", (req, res) => {
   });
 });
 
-router.post("/login", async (req, res) => {
+router.post("/login", loginValidators, async (req, res) => {
   try {
-    const { email, password } = req.body;
-
-    const candidate = await User.findOne({ email });
-    if (candidate) {
-      const areSame = await bcrypt.compare(password, candidate.password);
-      if (areSame) {
-        req.session.user = candidate;
-        req.session.isAuthenticated = true;
-        req.session.save((err) => {
-          if (err) throw err;
-          res.redirect("/");
-        });
-      } else {
-        req.flash("errorLogin", "Incorrect password");
-        res.redirect("/auth/login#login");
-      }
-    } else {
-      req.flash("errorLogin", "No user found. Register");
-      res.redirect("/auth/login#login");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash("errorLogin", errors.array()[0].msg);
+      return res.status(422).redirect("/auth/login#login");
     }
   } catch (e) {
     console.log(e);
@@ -55,8 +40,6 @@ router.post("/register", registerValidators, async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors.array());
-
       req.flash("errorRegister", errors.array()[0].msg);
       return res.status(422).redirect("/auth/login#register");
     }
--- utils/validators.js
+++ utils/validators.js
@@ -1,6 +1,7 @@
 const { body } = require("express-validator");
 
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.registerValidators = [
   body("email")
@@ -31,5 +32,37 @@ exports.registerValidators = [
     .trim(),
   body("name", "Enter valid name: minimum 3 symbols")
     .isLength({ min: 3 })
+    .trim(),
+];
+exports.loginValidators = [
+  body("email").isEmail().withMessage("Enter valid email").normalizeEmail(),
+  body("password", "Enter valid password: 6-56 symbols")
+    .isLength({ min: 6, max: 56 })
+    .isAlphanumeric()
+    .custom(async (value, { req }) => {
+      try {
+        const candidate = await User.findOne({ email: req.body.email });
+
+        if (!candidate) {
+          return Promise.reject("No user found. Register first");
+        }
+
+        if (candidate) {
+          const areSame = await bcrypt.compare(value, candidate.password);
+          if (!areSame) {
+            return Promise.reject("Incorrect password");
+          } else {
+            req.session.user = candidate;
+            req.session.isAuthenticated = true;
+            req.session.save((err) => {
+              if (err) throw err;
+              req.res.redirect("/");
+            });
+          }
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    })
     .trim(),
 ];
